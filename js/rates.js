35fcf37ab50a145d190100416bf5a6a28302fc66
iHWJ <95023111+iHWJ@users.noreply.github.com>
2025-12-31 15:19:22 +0800

    Update rates.js

--- js/rates.js
+++ js/rates.js
@@ -7,13 +7,13 @@ $httpClient.get(url, function(error, response, data) {
   }
 
   const rates = JSON.parse(data).rates;
-  const usdToCny = (1 / rates.USD).toFixed(2);
-  const cnyToHkd = rates.HKD.toFixed(2);
-  const cnyToJpy = rates.JPY.toFixed(2);
-  const cnyToKrw = rates.KRW.toFixed(2);
-  const eurToCny = (1 / rates.EUR).toFixed(2);
-  const gbpToCny = (1 / rates.GBP).toFixed(2);
-  const tryToCny = rates.TRY.toFixed(2);
+  const usdToCNY = (1 / rates.USD).toFixed(2);
+  const cnyToHKD = rates.HKD.toFixed(2);
+  const cnyToJPY = rates.JPY.toFixed(2);
+  const cnyToKRW = rates.KRW.toFixed(2);
+  const eurToCNY = (1 / rates.EUR).toFixed(2);
+  const gbpToCNY = (1 / rates.GBP).toFixed(2);
+  const tryToCNY = rates.TRY.toFixed(2);
   const timestamp = new Date().toLocaleTimeString("en-US", {
     hour: "2-digit",
     minute: "2-digit",
@@ -21,13 +21,13 @@ $httpClient.get(url, function(error, response, data) {
   });
 
   const content = `
-🇺🇸1美元兑 ${usdToCny}🇨🇳人民币
-🇨🇳1人民币兑 ${cnyToHkd}🇭🇰港币
-🇨🇳1人民币兑 ${cnyToJpy}🇯🇵日元
-🇨🇳1人民币兑 ${cnyToKrw}🇰🇷韩元
-🇨🇳1人民币兑 ${tryToCny}🇹🇷里拉
-🇪🇺1欧元兑 ${eurToCny}🇨🇳人民币
-🇬🇧1英镑兑 ${gbpToCny}🇨🇳人民币
+🇺🇸1美元兑 ${usdToCNY}🇨🇳人民币
+🇨🇳1人民币兑 ${cnyToHKD}🇭🇰港币
+🇨🇳1人民币兑 ${cnyToJPY}🇯🇵日元
+🇨🇳1人民币兑 ${cnyToKRW}🇰🇷韩元
+🇨🇳1人民币兑 ${tryToCNY}🇹🇷里拉
+🇪🇺1欧元兑 ${eurToCNY}🇨🇳人民币
+🇬🇧1英镑兑 ${gbpToCNY}🇨🇳人民币
 `;
 
   const panel = {
